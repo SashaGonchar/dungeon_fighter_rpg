c75a1ac811bf9d82117a34556a448ca34e08af23
SashaGonchar <nekospoon228@gmail.com>
2025-08-24 00:22:31 +0300

    fixed some bugs in routing, added index: true to home page, added 404 page.

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,21 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./Home";
-import Character from "./Character";
-import Shop from "./Shop";
-import Fight from "./Fight";
-import Profile from "./Profile";
+import Layout from "../pages/Layout.jsx";
+import Home from "../pages/Home.jsx";
+import Character from "../pages/Character.jsx";
+import Shop from "../pages/Shop.jsx";
+import Fight from "../pages/Fight.jsx";
+import Profile from "../pages/Profile.jsx";
+
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Layout />,
+        element: <Layout/>,
         children: [
-            { path: "/", element: <Home /> },
-            { path: "/character", element: <Character /> },
-            { path: "/shop", element: <Shop /> },
-            { path: "/fight", element: <Fight /> },
-            { path: "/profile", element: <Profile /> },
+            { index: true, element: <Home /> },
+            { path: "character", element: <Character /> },
+            { path: "shop", element: <Shop /> },
+            { path: "fight", element: <Fight /> },
+            { path: "profile", element: <Profile /> },
+            {
+                path: "*",
+                element: <h1>404: Please use only existing path, page not found</h1>,
+            },
         ],
     },
 ]);
